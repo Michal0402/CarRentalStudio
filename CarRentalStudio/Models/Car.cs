0253e66f6208701881e4c323293e40e41d29ba09
Filip 138453 <pzx110526@student.poznan.merito.pl>
2024-12-04 19:48:20 +0100

    Wprowadzenie modeli

---
+++ CarRentalStudio/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace CarRentalStudio.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateOnly Year { get; set; }
+        public float Mileage { get; set; }
+        public int HorsePower { get; set; }
+        public bool IsRented{ get; set;} // sprawdzić poźniej czy będzie działać i czy lepiej zrobić w kalendarzu
+        public int Price { get; set; }
+
+        //public virtual ICollection<Rent> Rents { get; set; }
+
+    }
+}
---
+++ CarRentalStudio/Models/Rating.cs
@@ -0,0 +1,12 @@
+﻿namespace CarRentalStudio.Models
+{
+    public class Rating
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Mark { get; set;}
+
+       
+    }
+}
